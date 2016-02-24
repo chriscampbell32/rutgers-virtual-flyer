1da274ede03695cdba966790e4d07c198f950800
chriscampbell32 <chriscampbell32@yahoo.com>
2016-02-24 15:38:17 -0500

    created views and layouts folders, and main.handlebars

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-//express
+//npm packages//
 var express = require('express');
 var expressHandlebars = require('express-handlebars');
+var Sequelize = require('sequelize');
+var passport = require('passport');
+var passportLocal = require('passport-local');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs')
 var app = express();
 var PORT = process.env.NODE_ENV || 8080;
 
+//sequelize database setup//
 
+//set up handlebars layout
+app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 
 app.listen(PORT, function(){
