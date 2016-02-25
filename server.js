cd81baba43fa62c5e61ab5951a034f136b4130dd
chriscampbell32 <chriscampbell32@yahoo.com>
2016-02-25 14:53:51 -0500

    added session package, and middleware, separated a couple packages from the top

--- server.js
+++ server.js
@@ -1,21 +1,49 @@
 //npm packages//
 var express = require('express');
 var expressHandlebars = require('express-handlebars');
-var Sequelize = require('sequelize');
-var passport = require('passport');
-var passportLocal = require('passport-local');
 var bodyParser = require('body-parser');
 var bcrypt = require('bcryptjs')
 var app = express();
 var PORT = process.env.NODE_ENV || 8080;
 
 //sequelize database setup//
+var Sequelize = require('sequelize');
+var connection = new Sequelize('rutgersflyers_db', 'root');
+
+//passport
+var passport = require('passport');
+var passportLocal = require('passport-local');
+
+//middleware
+app.use(require('express-session')({
+    secret: "supersecret",
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: (1000 * 60 * 60 *24 * 14)
+    },
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 
 //set up handlebars layout
 app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+//set up routes
+
 
 app.listen(PORT, function(){
     console.log("listening on port %s", PORT);
 });
+
+// // database connection via sequelize
+// connection.sync().then(function() {
+//   app.listen(PORT, function() {
+//       console.log("Listening on:" + PORT)
+//   });
+// });
