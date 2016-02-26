52635960098be1c93ce4e90bcf4eba20a65f54d8
chriscampbell32 <chriscampbell32@yahoo.com>
2016-02-25 19:55:11 -0500

    heroku create

--- server.js
+++ server.js
@@ -7,43 +7,43 @@ var app = express();
 var PORT = process.env.NODE_ENV || 8080;
 
 //sequelize database setup//
-var Sequelize = require('sequelize');
-var connection = new Sequelize('rutgersflyers_db', 'root');
+// var Sequelize = require('sequelize');
+// var connection = new Sequelize('rutgersflyers_db', 'root');
 
-//passport
-var passport = require('passport');
-var passportLocal = require('passport-local');
+// //passport
+// var passport = require('passport');
+// var passportLocal = require('passport-local');
 
-//middleware
-app.use(require('express-session')({
-    secret: "supersecret",
-    resave: true,
-    saveUninitialized: true,
-    cookie: {
-        secure: false,
-        maxAge: (1000 * 60 * 60 *24 * 14)
-    },
-}));
+// //middleware
+// app.use(require('express-session')({
+//     secret: "supersecret",
+//     resave: true,
+//     saveUninitialized: true,
+//     cookie: {
+//         secure: false,
+//         maxAge: (1000 * 60 * 60 *24 * 14)
+//     },
+// }));
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }));
 
 
-//set up handlebars layout
-app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+// //set up handlebars layout
+// app.engine('handlebars', expressHandlebars({defaultLayout: 'main'}));
+// app.set('view engine', 'handlebars');
 
 //set up routes
 
+app.listen(PORT, function(){
+    console.log("listening in port %s", PORT);
+});
 
-// app.listen(PORT, function(){
-//     console.log("listening on port %s", PORT);
-// });
 
 // database connection via sequelize
-connection.sync().then(function() {
-  app.listen(PORT, function() {
-      console.log("Listening on:" + PORT)
-  });
-});
+// connection.sync().then(function() {
+//   app.listen(PORT, function() {
+//       console.log("Listening on:" + PORT)
+//   });
+// });
