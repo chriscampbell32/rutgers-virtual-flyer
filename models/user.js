f58a8dd1f4cdda000030fd9bd2f065280a4d9036
Servio <servio19@gmail.com>
2016-02-27 12:26:41 -0500

    new structure for repo

---
+++ models/user.js
@@ -0,0 +1,33 @@
+var Sequelize = require('sequelize');
+var connection = new Sequelize('rutgersflyers_db', 'root');
+
+var User = connection.define('User', {
+  firstname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: false
+  },
+  lastname: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      len: {
+        args: [5,10],
+        msg: "Your password must be between 5-10 characters"
+      },
+    }
+  },
+});
+
+
+module.exports = User;
