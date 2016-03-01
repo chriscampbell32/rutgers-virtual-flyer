251816c8d4b7f1182a43fcd3a6f0f1901d95a584
ctevnan <carolyntevnan@yahoo.com>
2016-02-29 20:47:16 -0500

    will begin testing now that passport is in

--- server.js
+++ server.js
@@ -33,29 +33,69 @@ app.set('view engine', 'handlebars');
 
 app.use('/public', express.static(__dirname + "/public"));
 
+//using passport authenticate, specifying local strategy
+//to authenticate requests
+app.post('/login',
+  passport.authenticate('local',{ failureRedirect: '/login'}),
+  function(req,res) {
+    res.redirect('/');
+});
 
 //passport
 var passport = require('passport');
 var passportLocal = require('passport-local').Strategy;
 
 //config local strategy for passport
-//the local strategy needs a verify function that
+//the local strategy needs a verify callback that accepts credentials and calls done 
+//providing a user
 //gets the credentials given by the user (username,psswrd)
 
-passport.use(new Strategy(
-  function(username, password, cb) {
-    db.users.findByUsername(username, function(err, user) {
-      if (err) { return cb(err); }
-      if (!user) { return cb(null, false); }
-      if (user.password != password) { return cb(null, false); }
-      return cb(null, user);
+//this strategy takes an optional options hast before the function
+//both of the below fields define the name of the  properties in the POST body that
+//are sent to the server
+new LocalStrategy({/userNameField, /passwordField})
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+     if (err) { return done(err); }
+     if (!user) { return done(null, false); }
+     if (!user.verifyPassword(password)) { return done(null, false); }
+     return done(null, user);
     });
-  }));
+  }
+)};  
+
+//passport.use(new Strategy(
+//  function(username, password, cb) {
+//    db.users.findByUsername(username, function(err, user) {
+//      if (err) { return cb(err); }
+//      if (!user) { return cb(null, false); }
+//      if (user.password != password) { return cb(null, false); }
+//      return cb(null, user);
+//    });
+//  }));
 
 //config passport authenticated session persistence
 //passport must serialize users into and deserialize users out of
 //the session. just supply user ID when serializing and query the 
 //user record by ID from the db when deserializing
+
+//parameters by default localstrategy expect to find credentials
+//in parameters names username and password.
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'passwrd',
+  passReqToCalback: true,
+  session: false
+},
+function(req, username, password, done) {
+  //request object is the first argument now
+  //...
+}
+));
+
 
 passport.serializeUser(function(user, cb) {
   cb(null, user.id);
